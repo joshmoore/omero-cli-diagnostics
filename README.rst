a1db617558da4271f338e0a83a1fa01449502be6
jmoore <josh@glencoesoftware.com>
2017-09-07 17:06:31 +0200

    Top-level `bin/omero diagnostics` command
    This command loops through all sub-diagnostics
    commands and prints the relevant information.
    
    See https://github.com/dominikl/openmicroscopy/pull/9
    for the original proposal

---
+++ README.rst
@@ -0,0 +1,39 @@
+.. image:: https://travis-ci.org/ome/omero-cli-diagnostics.svg?branch=master
+    :target: https://travis-ci.org/ome/omero-cli-diagnostics
+
+.. image:: https://badge.fury.io/py/omero-cli-diagnostics.svg
+    :target: https://badge.fury.io/py/omero-cli-diagnostics
+
+omero-cli-diagnostics
+===================
+
+Plugin for use in the OMERO CLI.
+
+Requirements
+============
+
+* OMERO 5.4.0 or newer
+* Python 2.6+
+
+
+Installing from PyPI
+====================
+
+This section assumes that an OMERO.py is already installed.
+
+Install the app using `pip <https://pip.pypa.io/en/stable/>`_:
+
+::
+
+    $ pip install -U omero-cli-diagnostics
+
+License
+-------
+
+This project, similar to many Open Microscopy Environment (OME) projects, is
+licensed under the terms of the GNU General Public License (GPL) v2 or later.
+
+Copyright
+---------
+
+2017, The Open Microscopy Environment
---
+++ src/omero/plugins/diagnostics.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2017 University of Dundee & Open Microscopy Environment.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+   Plugin for printing all diagnostic messages
+"""
+
+import sys
+
+from omero.cli import CLI
+from omero.cli import BaseControl
+from omero.cli import DiagnosticsControl
+
+HELP = """Call diagnostics on all subplugins"""
+
+
+class CollectingDiagnosticsControl(BaseControl):
+    """
+    """
+
+    def _configure(self, parser):
+        self.__parser__ = parser  # For formatting later
+        parser.set_defaults(func=self.__call__)
+        parser.add_argument(
+            "--no-logs", action="store_true",
+            help="Skip log parsing")
+        # Argument list must match that of the
+        # DiagnosticsControl._add_diagnostics method
+
+    def __call__(self, args):
+        for name, control in sorted(self.ctx.controls.items()):
+            if isinstance(control, DiagnosticsControl):
+                control.diagnostics(args)
+
+try:
+    register("diagnostics", CollectingDiagnosticsControl, HELP)
+except NameError:
+    if __name__ == "__main__":
+        cli = CLI()
+        cli.register("diagnostics", CollectingDiagnosticsControl, HELP)
+        cli.invoke(sys.argv[1:])
